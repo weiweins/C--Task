b1422a9149d9abbe43e47e199e9586939d3cc679
weiweins <wsjo3jo3@gmail.com>
2025-03-12 11:38:08 +0800

    reset main

---
+++ Ch1/Ch1_code/Ch1_code/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch1_code
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+           
+        }
+    }
+}
